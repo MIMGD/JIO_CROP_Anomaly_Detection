07f989ffcd78ccb354d4dd3b5101811bafe217d9
Mohua <57787924+MS-2019-Git@users.noreply.github.com>
2022-08-14 23:29:49 +0530

    Added
    Added

---
+++ Deployment/Crop-Anomaly-Detection-App/app.py
@@ -0,0 +1,30 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import tensorflow as tf
+
+app = Flask(__name__)
+#model = pickle.load(open('model.pkl', 'rb'))
+model=tf.saved_model.load('saved_model')
+
+
+@app.route('/')
+def home():
+    return render_template('index_upload.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index_upload.html',format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
